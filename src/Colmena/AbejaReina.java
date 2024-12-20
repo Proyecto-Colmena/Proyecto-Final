bf915eae0be385921624acd12e3e40b595fbe2c8
Emilio292000 <emiliootero2000@hotmail.com>
2024-12-20 17:27:54 -0500

    Update AbejaReina.java

--- src/Colmena/AbejaReina.java
+++ src/Colmena/AbejaReina.java
@@ -1,4 +1,11 @@
 package Colmena;
 
-public class AbejaReina {
+public class AbejaReina extends Abeja {
+  public AbejaReina(String nombre){
+    super(nombre, "Abeja Reina");
+    
+  }
+  public void supervisarColmena() {
+    System.out.println(nombre + "Supervisa el trabajo de las abejas obreras. ")
+  }
 }
