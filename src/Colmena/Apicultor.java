b3b85482a9b16afae2023c9c354bd6bdd73e5a07
DanielaInsturainProg <daniela.insturain@uip.edu.pa>
2024-12-20 23:10:33 -0500

    Update Apicultor.java

--- src/Colmena/Apicultor.java
+++ src/Colmena/Apicultor.java
@@ -1,5 +1,16 @@
 package Colmena;
+import java.io.Serializable;
+//Clase para representar un apicultor
+public class Apicultor implements Serializable {
+    public String nombre;
+    public String telefono;
 
-public class Apicultor {
-    // Clase vacía para representar un apicultor
+    public Apicultor(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
 }
