7f41cf61f20d0d85c347efab85812b41aff0a7f0
DHerrer4 <hdl2005.21@gmail.com>
2024-12-20 20:11:51 -0500

    Add files via upload

--- src/Colmena/ExepcionesApicultura.java
+++ src/Colmena/ExepcionesApicultura.java
@@ -1,17 +1,16 @@
 package Colmena;
 
 public class ExcepcionesApicultura {
-
+    // Excepción para colmenas inválidas
     public static class ColmenaInvalidaException extends Exception {
         public ColmenaInvalidaException(String mensaje) {
             super(mensaje);
         }
     }
-
+    // Excepción para apicultores inválidos
     public static class ApicultorInvalidoException extends Exception {
         public ApicultorInvalidoException(String mensaje) {
             super(mensaje);
         }
     }
 }
-
