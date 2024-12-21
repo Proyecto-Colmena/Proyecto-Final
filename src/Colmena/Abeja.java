7ceb189d3edd8e9c0972bc455e936c48e41f464f
DanielaInsturainProg <daniela.insturain@uip.edu.pa>
2024-12-20 23:09:17 -0500

    Update Abeja.java

--- src/Colmena/Abeja.java
+++ src/Colmena/Abeja.java
@@ -1,4 +1,15 @@
 package Colmena;
-//Clase para representar abejas
-public class Abeja {
+
+import java.io.Serializable;
+
+public class Abeja implements Serializable {
+    public String estadoSalud;
+
+    public Abeja(String estadoSalud) {
+        this.estadoSalud = estadoSalud;
+    }
+
+    public String getInfo() {
+        return String.format("❤️ Estado de Salud: %s%n", estadoSalud);
+    }
 }
