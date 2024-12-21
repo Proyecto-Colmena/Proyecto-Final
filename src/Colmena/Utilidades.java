fa193ee6c42566a9270541867623de6d1097394e
Fl2on <69091361+fl2on@users.noreply.github.com>
2024-12-21 09:46:37 -0500

    Update Utilidades.java

--- src/Colmena/Utilidades.java
+++ src/Colmena/Utilidades.java
@@ -75,7 +75,7 @@ public class Utilidades {
                 for (String frame : frames) {
                     System.out.print("\r" + frame); // Imprimimos el cuadro de animación
                     try {
-                        Thread.sleep(300) // Espera constante de 300 ms
+                        Thread.sleep(300); // Espera constante de 300 ms
                     } catch (InterruptedException ignored) {}
                 }
             }
