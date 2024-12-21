541210755ab19e1b5703afeb2438124bef56c395
luissanchez07 <luisromerosanchez07@gmail.com>
2024-12-21 00:09:52 -0500

    Update AbejaReina.java

--- src/Colmena/AbejaReina.java
+++ src/Colmena/AbejaReina.java
@@ -1,11 +1,20 @@
 package Colmena;
 
 public class AbejaReina extends Abeja {
-  public AbejaReina(String nombre){
+  
+  // Constructor
+  public AbejaReina(String nombre) {
     super(nombre, "Abeja Reina");
-    
   }
+
+  // Método específico de la abeja reina
   public void supervisarColmena() {
-    System.out.println(nombre + " Supervisa el trabajo de las abejas obreras. "); // Supervisión
+    System.out.println(getNombre() + " supervisa el trabajo de las abejas obreras y asegura el orden en la colmena.");
+  }
+
+  // Sobrescritura del método trabajar
+  @Override
+  public void trabajar() {
+    System.out.println(getNombre() + " no trabaja físicamente, pero supervisa la colmena.");
   }
 }
