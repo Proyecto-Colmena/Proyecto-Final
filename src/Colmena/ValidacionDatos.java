91b58c8df0586ee104bd2a0974459e1f71a7bff7
Aleccito <alexanderxrv07@gmail.com>
2024-12-20 20:24:22 -0500

    Update ValidacionDatos.java

--- src/Colmena/ValidacionDatos.java
+++ src/Colmena/ValidacionDatos.java
@@ -2,16 +2,17 @@ package Colmena;
 
 public class ValidacionDatos {
 
-    public static void validarCapacidadColmena(int capacidad) throws IllegalArgumentException {
+    // Valida que la capacidad de la colmena sea mayor a 0
+    public static void validarCapacidadColmena(int capacidad) {
         if (capacidad <= 0) {
             throw new IllegalArgumentException("La capacidad de la colmena debe ser mayor a 0.");
         }
     }
 
-    public static void validarNombre(String nombre) throws IllegalArgumentException {
+    // Valida que el nombre no sea nulo ni esté vacío
+    public static void validarNombre(String nombre) {
         if (nombre == null || nombre.isBlank()) {
             throw new IllegalArgumentException("El nombre no puede estar vacío o nulo.");
-            
         }
     }
 }
