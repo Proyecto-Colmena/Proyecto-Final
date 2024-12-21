b579b2bf5398d68c02d95b95870f80553dfa9f84
Fl2on <69091361+fl2on@users.noreply.github.com>
2024-12-21 00:23:00 -0500

    Fix: DatosApicola.java

--- src/Colmena/DatosApicola.java
+++ src/Colmena/DatosApicola.java
@@ -1,6 +1,16 @@
 package Colmena;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
+public class DatosApicola implements Serializable {
+    public List<Colmena> colmenas;
+    public List<Apicultor> apicultores;
+    public Map<String, Apicultor> asignaciones;
 
-
-public class DatosApicola {
+    public DatosApicola(List<Colmena> colmenas, List<Apicultor> apicultores, Map<String, Apicultor> asignaciones) {
+        this.colmenas = colmenas;
+        this.apicultores = apicultores;
+        this.asignaciones = asignaciones;
+    }
 }
